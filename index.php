7151e4466037fd874b47034d227521b3fefa5a4f
Shohei Yokoyama <github@lab.yokoyama.ac>
2018-02-07 19:44:33 +0900

    add a-text

--- index.php
+++ index.php
@@ -9,18 +9,26 @@ if(isset($_GET['img'])){
     }
     $text = "";
 	if(isset($_GET['text'])){
-        $text = $_GET['text'];
+        $text = '<a-text font="monoid" value="'.$_GET['text'].'" width="5" position="-2.5 0.25 -2.0" rotation="10 10 0"></a-text>';
     }
 	$bar = <<<EOT
 <html>
 <head></head>
 <body>
-<a-scene><a-sky src="{$img}"><a-animation attribute="rotation" fill="forwards" easing="linear" dur="200000" from="0 {$angle} 0" to="0 {$angleLast} 0" repeat="indefinite"></a-animation></a-sky></a-scene><script src="https://aframe.io/releases/0.7.0/aframe.min.js"></script>;
+<a-scene>
+{$text}
+<a-sky src="{$img}"><a-animation attribute="rotation" fill="forwards" easing="linear" dur="200000" from="0 {$angle} 0" to="0 {$angleLast} 0" repeat="indefinite"></a-animation></a-sky></a-scene><script src="https://aframe.io/releases/0.7.0/aframe.min.js"></script>;
 </body>
 </html>
 EOT;
 echo $bar;
 }else{
-    echo "hello";
+    $bar = <<<EOT
+<html>
+<head></head>
+<body>&nbsp;</body>
+</html>
+EOT;
+    echo $bar;
 }
 ?>
